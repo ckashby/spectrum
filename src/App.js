7e8250db77499e21b407eb13363ffe6fd3704680
ckashby <clay.ashby@gmail.com>
2020-10-22 03:31:34 -0700

    Refactored Posts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import axios from "axios";
-import Post from "./components/Post/Post";
 import Posts from './components/Posts/Posts';
 import firebase from '@firebase/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Event: ", event);
-  };
-
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      setPosts(response.data);
-      console.log("Got it!");
-    })
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Hola</h1>
-      <p>Lorem ipsum morende kavino ono marco.</p>
-      <ul>
-      {posts.map(post => {
-        return (
-          <>
-          <Post id={post.id} title={post.title} /><p>{post.body}</p>
-          {/* <li id={post.id}>{post.title}</li> */}
-          </>
-        )
-
-      })}
-      </ul>
-    </div>
-  );
+    <>
+    <h1>Main Menu</h1>
+    <Posts />
+    </>
+  )
 }
 
 export default App;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Post from '../Post/Post';
 
-export const Posts = () => {
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      setPosts(response.data);
+      console.log("Got it!");
+    })
+  }, []);
+
   return (
     <div>
-      <h2>We are the posts.</h2>
+      <h2>Posts</h2>
+      <ul>
+      {posts.map(post => {
+        return (
+          <>
+          <Post id={post.id} title={post.title} /><p>{post.body}</p>
+          </>
+        )
+      })}
+      </ul>
     </div>
   )
 }
+
+export default Posts;
+
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log("Event: ", event);
+  // };
