d7f80836533d190bab1c634519a32e3cb5256e57
ckashby <clay.ashby@gmail.com>
2020-10-24 10:53:51 -0700

    added Header for navigation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import firebase from "@firebase/app";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 import "./App.css";
-import MenuPage from "./components/pages/MenuPage";
+// import MenuPage from "./components/pages/MenuPage";
+import Header from "./components/Header/Header";
 import Posts from "./components/Posts/Posts";
 import Albums from "./components/Albums/Albums";
 import Users from "./components/Users/Users";
@@ -14,7 +15,7 @@ function App() {
     <>
       <BrowserRouter>
         {/* <Switch> */}
-        <MenuPage />
+        <Header />
         <Route path="/posts" exact component={Posts} />
         <Route path="/albums" exact component={Albums} />
         <Route path="/users" exact component={Users} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuPage from "../pages/MenuPage";
+
+function Header() {
+  return (
+    <div>
+      <MenuPage />
+    </div>
+  );
+}
+
+export default Header;
